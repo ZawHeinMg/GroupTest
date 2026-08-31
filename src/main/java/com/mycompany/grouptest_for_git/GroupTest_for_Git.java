9db5520bb06edead483ca559baab2f0403ebd69c
ZawHeingMg <zawheinmaung@mail.arakkha.net>
2026-08-31 13:28:44 +0630

    add project

---
+++ src/main/java/com/mycompany/grouptest_for_git/GroupTest_for_Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grouptest_for_git;
+
+/**
+ *
+ * @author KoukChay
+ */
+public class GroupTest_for_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
